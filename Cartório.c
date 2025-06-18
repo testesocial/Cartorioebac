98d7975d67dd63c675f4a1d2d3341a446fc6a5ce
testesocial <lelekolilian@gmail.com>
2025-06-18 15:16:24 -0300

    Opção de saída
    Incluso a opção de saída para o usuário

--- Cartório.c
+++ Cartório.c
@@ -122,9 +122,10 @@ int main()
 	    setlocale(LC_ALL,"Portuguese"); //definindo a linguagem
     	printf("### Cartório da EBAC ### \n\n"); //inicio do menu
     	printf("escolha sua opção desejada no menu\n\n");
-    	printf("\t1 registrar nomes\n"); 
-    	printf("\t2 consultar nomes\n");
-    	printf("\t3 deletar nomes\n\n");
+    	printf("\t1 - Registrar nomes\n"); 
+    	printf("\t2 - Consultar nomes\n");
+    	printf("\t3 - Deletar nomes\n\n");
+    	printf("\t4 - Sair do sistema\n\n");
     	printf("Opção:"); //final do menu
 	
 	    scanf("%d", &opcao); //armazenando a escolha do usuário
@@ -145,7 +146,11 @@ int main()
 	       case 3:	
 	       deletar();	
 	       break;
-	    
+	       
+	       case 4:
+	       printf("Obrigado por utilizar o sistema!\n");
+		   return 0;
+		   break;	    
 	    
 	       default:
 	       printf("Essa opção não está disponivel!\n");	
